ed74c9d09081c737058b03f42a4868a98ff85f76
strawberry <strawberry.wu@newtype.io>
2019-10-21 15:36:26 +0800

    first commit

---
+++ admin/src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Main from './views/Main.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: Main,
+      children:[]
+    }
+  ]
+})
+
+export default router
