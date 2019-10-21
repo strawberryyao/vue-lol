5be531486dd6ba95a4d4965d4115da32b6a344b7
strawberry <strawberry.wu@newtype.io>
2019-10-21 17:40:05 +0800

    新建分类列表

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from './views/Main.vue'
+import CategoryEdit from './views/CategoryEdit.vue';
+import CategoryList from './views/CategoryList.vue';
 
 Vue.use(Router)
 
@@ -10,7 +12,10 @@ const router = new Router({
       path: '/',
       name: 'main',
       component: Main,
-      children:[]
+      children:[
+        { path: '/categories/create', component: CategoryEdit },
+        { path: '/categories/list', component: CategoryList }
+      ]
     }
   ]
 })
---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="category">
+    <h1>新建分类</h1>
+    <el-form @submit.native.prevent="save">
+      <el-form-item label="分类名称">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        model: '',
+      }
+    },
+    methods: {
+      save () {
+        alert(1111)
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
